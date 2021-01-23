321c89c516b2941d91106f685233d24e15b739fb
Ryan McNair <ryan.mcnair@gmail.com>
2021-01-22 18:49:08 -0600

    put building into it's own file

---
+++ Urban Planner/Building.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Urban_Planner
+{
+    class Building
+    {
+        //Properties
+        private string _designer { get; set; }
+        private DateTime _dateConstructed { get; set; } = DateTime.Now;
+        private string _address { get; set; }
+        private string _owner { get; set; }
+
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Volume => Width * Height * (3 * Stories);
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+        public DateTime Construct() => _dateConstructed;
+
+        public string Purchase(string newOwner)
+        {
+            _owner = newOwner;
+            return _owner;
+        }
+
+        public string Designer(string newDesigner)
+        {
+            _designer = newDesigner;
+            return _designer;
+        }
+
+        public string Address() => _address;
+
+    }
+}
